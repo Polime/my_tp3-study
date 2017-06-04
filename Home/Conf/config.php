63b0eba87ed4c5ba9303947a2ace2ae5dfca2926
Polime <530875962@qq.com>
2017-06-04 20:46:31 +0800

    update

---
+++ Home/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'DB_DSN'=>'mysql://root:root@localhost:3306/db_blog',
+    'DB_PREFIX'=>'tb_',
+    'SHOW_PAGE_TRACE'=>true,
+    'TMPL_L_DELIM'=>'<{',
+    'TMPL_R_DELIM'=>'}>',
+);
+?>
